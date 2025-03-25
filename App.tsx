9762cd46b78e04d2e5dc567ed4c5245fe6b2c74e
Shubham Mishra <shubhamkingsmenrealty@gmail.com>
2025-03-25 15:34:32 +0530

    Update

--- App.tsx
+++ App.tsx
@@ -90,7 +90,7 @@ function App(): React.JSX.Element {
             paddingBottom: safePadding,
           }}>
           <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+            Edit <Text style={styles.highlight}>Hello</Text> to change this
             screen and then come back to see your edits.
           </Section>
           <Section title="See Your Changes">
